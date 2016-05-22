aad163d52cc2ec4672b1474504490e63e1cf24e2
Daniel Mack <daniel@zonque.org>
2016-05-22 12:36:50 +0200

    wifi-udp: add 2nd state machine as demo

--- arduino/wifi-udp/wifi-udp.ino
+++ arduino/wifi-udp/wifi-udp.ino
@@ -7,22 +7,59 @@
 #define SERVER_IP "192.168.178.131"
 #define SERVER_PORT 2121
 
-static char wifiPacketPayload[32];
+static char wifiPacketPayload[64];
+static unsigned int pumpTime = 0;
 
 static const char *preparePayload(void) {
   static int seq = 0;
-  snprintf(wifiPacketPayload, sizeof(wifiPacketPayload), "Seq %d, Battery %d\n", seq++, analogRead(0));
+  snprintf(wifiPacketPayload, sizeof(wifiPacketPayload), "Seq %d, Battery %d, Tank %d, PumpTime %d\n", seq++, analogRead(0), analogRead(1), pumpTime);
+  pumpTime = 0;
   return wifiPacketPayload;
 }
 
 WifiStateMachine wifi(sw_serial_rx_pin, sw_serial_tx_pin, esp8266_reset_pin,
                       SERVER_IP, SERVER_PORT, preparePayload, Serial);
 
+class PumpStateMachine : public StateMachine
+{
+private:
+  void triggerState(int state) {
+    switch (state) {
+      case 0:
+        Serial.println("pump off");
+        digitalWrite(12, HIGH);
+        digitalWrite(5, HIGH);
+        waitState(1, 1 * MILLIS_PER_MINUTE);
+        break;
+      case 1:
+        Serial.println("pump on");
+        digitalWrite(12, LOW);
+        digitalWrite(5, LOW);
+        waitState(0, 30 * MILLIS_PER_SECOND);
+        pumpTime += 30 * MILLIS_PER_SECOND;
+        break;
+    }
+  }
+public:
+  PumpStateMachine() {
+    waitState(0, 0);
+  };
+};
+
+PumpStateMachine pump;
+
 void setup() {
   Serial.begin(115200);
   wifi.reset();
+
+  pinMode(5, OUTPUT);
+  pinMode(12, OUTPUT);
+
+  digitalWrite(5, HIGH);
+  digitalWrite(12, HIGH);
 }
 
 void loop() {
   wifi.tick();
+  pump.tick();
 }
